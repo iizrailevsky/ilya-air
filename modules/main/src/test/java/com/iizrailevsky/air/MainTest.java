7bba674e339b4a2b4a046685d3f0411f04c0c3c0
Ilya Izrailevsky <iizrailevsky@Ilyas-iMac.local>
2018-04-11 21:15:26 -0700

    initial checkin

---
+++ modules/main/src/test/java/com/iizrailevsky/air/MainTest.java
@@ -0,0 +1,13 @@
+package com.iizrailevsky.air;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testFoo() {
+        assertTrue(true);
+    }
+}
